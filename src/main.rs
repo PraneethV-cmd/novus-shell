82667e63275059ece1a3e8cfe30d98cc46e8d4c1
PraneethV-cmd <praneethv375@gmail.com>
2025-07-01 17:08:37 +0000

    added type PATH

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,93 @@
+#![allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
+use std::env;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let stdin = io::stdin();
+
     loop {
+        // Print prompt
         print!("$ ");
         io::stdout().flush().unwrap();
 
+        // Read input
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-
         let input = input.trim();
 
-        if input == "exit 0" {break}
-        let words:  Vec<&str> = input.split_whitespace().collect();
+        // Exit command
+        if input == "exit 0" {
+            break;
+        }
 
-        match words.split_first() {
-            Some((&"echo", rest)) => {
-                let output = rest.join(" ");
-                println!("{}", output);
+        // Tokenize
+        let words: Vec<&str> = input.split_whitespace().collect();
+
+        match words.as_slice() {
+            [""] => continue,
+            ["echo", args @ ..] => cmd_echo(args),
+            ["type", args @ ..] => cmd_type(args),
+            ["exit", "session"] => {
+                println!("Terminating the current session...");
+                exit(0);
             }
-            Some((cmd, _)) => {
-                println!("novus-shell: Command not found: {}", cmd);
-            }
-            None => {
-                continue
+            _ => {
+                if let Some(path) = explore_path(words[0]) {
+                    println!("{} is {}", words[0], path.display());
+                } else {
+                    println!("{}: command not found", words[0]);
+                }
             }
         }
     }
 }
 
+fn cmd_echo(args: &[&str]) {
+    println!("{}", args.join(" "));
+}
+
+fn cmd_type(args: &[&str]) {
+    if args.is_empty() {
+        return;
+    }
+
+    if args.len() > 1 {
+        println!("Too many arguments...");
+        return;
+    }
+
+    let cmd = args[0];
+
+    match cmd {
+        "type" | "echo" | "exit" => {
+            println!("{} is a shell builtin", cmd);
+        }
+        _ => {
+            if let Some(path) = explore_path(cmd) {
+                println!("{} is {}", cmd, path.display());
+            } else {
+                println!("{}: Command not found...", cmd);
+            }
+        }
+    }
+}
+
+fn explore_path<P>(exe_name: P) -> Option<PathBuf>
+where
+    P: AsRef<Path>,
+{
+    env::var_os("PATH").and_then(|paths| {
+        env::split_paths(&paths)
+            .filter_map(|dir| {
+                let full_path = dir.join(&exe_name);
+                if full_path.is_file() {
+                    Some(full_path)
+                } else {
+                    None
+                }
+            }).next()
+    })
+}
 
